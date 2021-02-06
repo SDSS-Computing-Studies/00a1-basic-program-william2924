213b8c0ab1247b4c9169b48af81a1b620b64f75e
william2924 <78515221+william2924@users.noreply.github.com>
2021-02-05 18:42:35 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -3,4 +3,6 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print("Hello world! This is how multiple print statements are joined.", end="")
+Hello world! This is how multiple print statements are joined.=a
+, end=""=b
+print("a*b")
