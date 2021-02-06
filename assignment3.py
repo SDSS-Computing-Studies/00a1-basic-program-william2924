26be0ef1d49dc5269410e2e5a87e0ca263659a3a
william2924 <78515221+william2924@users.noreply.github.com>
2021-02-05 16:53:00 -0800

    assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,16 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
+/"print('This is how multiple print statements are joined.Use end=""')
+
+/"x=This is how multiple print statements are joined=x
+
+/"y=Use end=""
+
+/'print("x**y")
+
+/"rint('This is how multiple print statements are joined.Use end=""')
+
 print('This is how multiple print statements are joined.Use end=""')
 x=/"This is how multiple print statements are joined=x
 y=/"Use end=""
