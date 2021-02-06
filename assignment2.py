659a696bfcc099ea9b047a470a87c20ec85d0586
william2924 <78515221+william2924@users.noreply.github.com>
2021-02-05 17:22:20 -0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,3 @@
-/"This is my second program.=x
-/'It uses "two commands" to display the output.=y
-print("x*y")
+print("This is my second program.")
+
+print('It uses "two commands" to display the output.')
