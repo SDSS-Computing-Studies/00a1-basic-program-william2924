7e9ee1b7d35969f53ac4d96924e3e0c78d2be70c
william2924 <78515221+william2924@users.noreply.github.com>
2021-02-05 17:20:52 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -3,11 +3,3 @@
 # SD Computing Studies Assignment
 
 print("Hello world")
-
-print("This is my second program.")
-
-print('It uses "two commands" to display the output.')
-
-print('This is how multiple print statements are joined.Use end=""')
-
-print('This is how multiple print statements are joined.Use end=""')
